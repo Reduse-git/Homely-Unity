fcc4a121ce0823f6d98623964dd66bbf500d987a
Reduse <den.reduse@gmail.com>
2020-07-01 16:43:45 +0300

    add controller and deth

---
+++ Assets/ReduseWork/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public int helth;
+
+    public float speed;
+    public GameObject main;
+    private Rigidbody2D rb;
+    private Vector2 mv;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector2 mi = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        mv = mi.normalized * speed;
+        
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + mv * Time.fixedDeltaTime);
+
+        if (helth <= 0)
+        {
+            destroy();
+        }
+    }
+
+    void destroy()
+    {
+        Destroy(main,.5f);
+    }
+}
